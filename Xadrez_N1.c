69a6309350686d0f1cbd4196e887048db4804aaa
LeHavoc <108563266+LeHavoc@users.noreply.github.com>
2025-10-31 22:47:48 -0300

    Update Xadrez_N1.c

--- Xadrez_N1.c
+++ Xadrez_N1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(){
+
+    int torre, bispo = 1, rainha = 1;
+
+    printf("movimento da torre:\n"); //simulação de movimento da torre 5 casas para a direita
+    for (torre = 1; torre <=5; torre++) {
+        printf("Torre moveu %d casa(s) para a direita.\n", torre);
+    }
+
+    printf("Movimento do bispo:\n"); //movimento do bispo 5 casas para a diagonal direita
+    while (bispo <= 5) {
+        printf("Bispo se moveu %d casa(s) para cima e %d casa(s) para a direita.\n" , bispo , bispo);
+        bispo++;
+    }
+
+    printf("Movimento da rainha:\n"); //movimento da rainha 8 casas para a esquerda
+    do {
+        printf("Rainha se moveu %d casa(s) para a esquerda.\n" , rainha);
+        rainha++;
+    } while (rainha <= 8);
+
+    return 0;
+}
